1c5018a051631f95a0a2916dfde617f4fd63efd5
Koen Bonnet <koen@goodgrid.nl>
2024-05-24 21:51:25 +0200

    Hidden files and config.js does not contain anything secret so can be tracked

---
+++ config.js
@@ -0,0 +1,14 @@
+
+const config = {
+    debug: process.env.debug,
+    twilioUrl: process.env.twilioUrl,
+    twilioUsername: process.env.twilioUsername,
+    twilioPassword: process.env.twilioPassword,
+    twilioPhoneNumber: process.env.twilioPhoneNumber,
+    recipientsPhoneNumbers: process.env.recipientsPhoneNumbers,
+    crlUrl: process.env.crlUrl,
+    opensslPath: process.env.opensslPath,
+    crlAcceptableLifetimeMinutes: process.env.crlAcceptableLifetimeMinutes
+}
+
+export default config
